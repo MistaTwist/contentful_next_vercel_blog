b31ea83e4668b594ad1ae42bcc506ca913e83ec9
Paul Perry <porlyp@gmail.com>
2020-09-14 08:42:12 +0100

    Update post.js

--- components/post.js
+++ components/post.js
@@ -19,6 +19,20 @@ export default function Post( { alt, date, image, title, url}) {
                     padding: 24px;
                     position: absolute;
                 }
+                a, a:hover {
+                    border-bottom: none;
+                }
+                h2 {
+                    color: white;
+                    font-size:24px;
+                    margin-bottom:0;
+                }
+                h4 {
+                    color:rgba(255, 255, 255, 0.8);
+                    font-size:18px;
+                    font-weight: 500;
+                    margin-top: 8px;
+                }
             `}                
             </style>
         </div>
